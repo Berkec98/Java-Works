c8ab50d7aaded81cbe47aef1fc9669291b8d3f67
Berke <76871439+Berkec98@users.noreply.github.com>
2021-04-09 00:34:18 +0300

    Update Main.java

--- Employee and Manager infos/src/ders2/Main.java
+++ Employee and Manager infos/src/ders2/Main.java
@@ -2,7 +2,7 @@ package ders2;
 
 public class Main {
 	public static void main(String[]args) {
-		Manager manager = new Manager("Berke","Yazılım",3000,5);
+		Manager manager = new Manager("Berke","Software",3000,5);
 		manager.showInfos();
 		manager.maasaZam(100);
 	}
